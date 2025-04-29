3d5200e203ec79d35700be01286dd357330428ed
Usuario <santiago.mayor@correounivalle.edu.co>
2025-04-28 22:20:58 -0500

    función definirManiobra terminada

--- app/src/main/scala/taller/MovimientosTren.scala
+++ app/src/main/scala/taller/MovimientosTren.scala
@@ -33,4 +33,44 @@ class MovimientosTren {
       acc :+ nuevoEstado
     }
   }
+  def definirManiobra(t1: Tren, t2: Tren): Maniobra = {
+    @tailrec
+    def aux(pendiente: Tren, objetivo: Tren, maniobra: Maniobra, estado: Estado): Maniobra = {
+      objetivo match {
+        case Nil => maniobra
+        case cabezaObjetivo :: restoObjetivo =>
+          val (principal, uno, dos) = estado
+          if (principal.nonEmpty && principal.head == cabezaObjetivo) {
+            aux(principal.tail, restoObjetivo, maniobra, (principal.tail, uno, dos))
+          } else {
+            if (principal.contains(cabezaObjetivo)) {
+              val index = principal.indexOf(cabezaObjetivo)
+              val movimientos = List(Uno(principal.length - index))
+              val nuevoEstado = aplicarMovimiento(estado, movimientos.head)
+              aux(nuevoEstado._1, objetivo, maniobra ++ movimientos, nuevoEstado)
+            } else if (uno.contains(cabezaObjetivo)) {
+              val index = uno.indexOf(cabezaObjetivo)
+              val movimientos = List(Uno(-(index + 1)))
+              val nuevoEstado = aplicarMovimiento(estado, movimientos.head)
+              aux(nuevoEstado._1, objetivo, maniobra ++ movimientos, nuevoEstado)
+            } else if (dos.contains(cabezaObjetivo)) {
+              val index = dos.indexOf(cabezaObjetivo)
+              val movimientos = List(Dos(-(index + 1)))
+              val nuevoEstado = aplicarMovimiento(estado, movimientos.head)
+              aux(nuevoEstado._1, objetivo, maniobra ++ movimientos, nuevoEstado)
+            } else {
+              if (principal.nonEmpty) {
+                val movimientos = List(Uno(1))
+                val nuevoEstado = aplicarMovimiento(estado, movimientos.head)
+                aux(nuevoEstado._1, objetivo, maniobra ++ movimientos, nuevoEstado)
+              } else {
+                maniobra
+              }
+            }
+          }
+      }
+    }
+    val estadoInicial = (t1, Nil, Nil)
+    aux(t1, t2, Nil, estadoInicial)
+  }
 }
